0b2434bf6bd11dc760c1389d9ef0db33802613cd
virus <cdromscsi@gmail.com>
2019-12-19 01:41:04 +0200

    fetch part 1

--- client/redux/reducers/users.js
+++ client/redux/reducers/users.js
@@ -1,7 +1,8 @@
 const GET_DATA = 'skillcrucial//users/GET_DATA'
+const ERROR_HAPPENED = 'skillcrucial//users/ERROR_HAPPENED'
 
 const InitialState = {
-  list: [{ name: 'test' }]
+  list: []
 }
 
 export default (state = InitialState, action) => {
@@ -9,18 +10,28 @@ export default (state = InitialState, action) => {
     case GET_DATA:
       return {
         ...state,
-        list: state.list.concat(action.list)
+        list: action.list
       }
     default:
       return state
   }
 }
 
-export function getData () {
+export function getData() {
   return (dispatch) => {
-    dispatch({
-      type: GET_DATA,
-      list: [{ name: +(new Date()) }]
-    })
+    return fetch('/api/users')
+      .then(res => res.json())
+      .then((json) => {
+        dispatch({
+          type: GET_DATA,
+          list: json
+        })
+      })
+      .catch((err) => {
+        dispatch({
+          type: ERROR_HAPPENED,
+          err
+        })
+      })
   }
 }
